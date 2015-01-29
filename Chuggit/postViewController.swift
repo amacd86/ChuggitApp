57b2cbed1846abad6d7f1d5f937b325377b427b2
Angus MacDonald <amacd86@gmail.com>
2015-01-28 21:17:09 -0500

    // Initial add of postViewController

---
+++ Chuggit/postViewController.swift
@@ -0,0 +1,36 @@
+//
+//  postViewController.swift
+//  Chuggit
+//
+//  Created by Angus MacDonald on 1/26/15.
+//  Copyright (c) 2015 Hutch Productions, LLC. All rights reserved.
+//
+
+import UIKit
+
+class postViewController: UIViewController {
+
+    
+    @IBOutlet var chuggToPost: UIImageView!
+    @IBAction func chooseChugg(sender: AnyObject) {
+    }
+    @IBOutlet var shareCaption: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        
+        
+        
+        
+    }
+}
